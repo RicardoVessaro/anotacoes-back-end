500f17811ff798a643cf38fd1cebc0bceae57ba6
ricardovessaro <ricardolvessaro@gmail.com>
2022-02-12 12:06:18 -0300

    criacao object util

---
+++ api/utils/object_util.py
@@ -0,0 +1,77 @@
+
+NOT_SUPPORTED_TYPES_EXCEPTION_MESSAGE = "The type '{0}' is not supported."
+
+NOT_SUPPORTED_TYPES = [
+    int, float, complex, range, set, frozenset,
+    bytes, bytearray, memoryview
+]
+
+def is_none_or_empty(value, verify_iterable_values=True):
+    _validate_type(value)
+
+    if value is None:
+        return True
+
+    elif value is bool:
+        return True
+    
+    elif type(value) is str:
+        return is_string_none_or_empty(value)
+
+    elif type(value) is list:
+        return is_list_none_or_empty(value, verify_iterable_values)
+
+    elif type(value) is tuple:
+        return is_tuple_none_or_empty(value, verify_iterable_values)
+
+    elif type(value) is dict:
+        return is_dict_none_or_empty(value, verify_iterable_values)
+
+def is_string_none_or_empty(string: str):
+    return len(string) == 0 or len(string.split()) == 0
+
+def is_list_none_or_empty(lizt: list,  verify_iterable_values=True):
+    if lizt == [] or len(lizt) == 0:
+        return True
+
+    if verify_iterable_values:
+        return _is_iterable_values_none_or_empty(lizt)
+
+    return False
+
+def is_tuple_none_or_empty(tvple: tuple,  verify_iterable_values=True):
+    if tvple == () or len(tvple) == 0:
+        return True
+
+    if verify_iterable_values:
+        return _is_iterable_values_none_or_empty(tvple)
+
+    return False
+
+def is_dict_none_or_empty(dct: dict, verify_iterable_values=True):
+    if dct == {} or len(dct) == 0:
+        return True
+
+    if verify_iterable_values:
+        return _is_dict_values_none_or_empty(dct)
+
+    return False
+
+def _is_iterable_values_none_or_empty(iterable):
+    for item in iterable:
+        if not is_none_or_empty(item):
+            return False
+    
+    return True
+
+def _is_dict_values_none_or_empty(dct: dict):
+    for key, value in dct.items():
+        if not is_none_or_empty(value):
+            return False
+
+    return True
+
+def _validate_type(value):
+    tipe = type(value) 
+    if tipe in NOT_SUPPORTED_TYPES:
+        raise Exception(NOT_SUPPORTED_TYPES_EXCEPTION_MESSAGE.format(tipe))
---
+++ tests/util/object_util_test.py
@@ -0,0 +1,94 @@
+from pytest import raises
+from api.utils.object_util import is_none_or_empty, NOT_SUPPORTED_TYPES, NOT_SUPPORTED_TYPES_EXCEPTION_MESSAGE
+
+def test_none_value():
+    assert is_none_or_empty(None)
+
+def test_boolean_value():
+    assert not is_none_or_empty(True)
+    assert not is_none_or_empty(False)
+
+def test_string_value():
+    assert is_none_or_empty("")
+    assert is_none_or_empty("    ")
+    assert not is_none_or_empty("None")
+
+def test_list_value():
+    assert is_none_or_empty([])
+    assert is_none_or_empty([[], []])
+    assert is_none_or_empty([
+        [], None, "", ["", None],  (),
+    ])
+
+    assert is_none_or_empty([], verify_iterable_values=False)
+    assert not is_none_or_empty([
+        [], None, "", ["", None],  (),
+    ], verify_iterable_values=False)
+    assert not is_none_or_empty([[], []], verify_iterable_values=False)
+    
+def test_tuple_value():
+    assert is_none_or_empty(())
+    assert is_none_or_empty(((), ()))
+    assert is_none_or_empty((
+        [], None, "", ("", None),  (),
+    ))
+
+    assert is_none_or_empty((), verify_iterable_values=False)
+    assert not is_none_or_empty((
+        [], None, "", ("", None),  (),
+    ), verify_iterable_values=False)
+    assert not is_none_or_empty(((), ()), verify_iterable_values=False)
+NOT_SUPPORTED_TYPES_EXCEPTION_MESSAGE
+def test_dict_value():
+    assert is_none_or_empty({})
+    assert is_none_or_empty({
+        "a": None, 
+        "b": [], 
+        "c": (),
+        "d": "",
+        "e": [[], ()],
+        "f": ((), []),
+        "g": {},
+        "h": {
+           "1" : None,
+           "2": {},
+           "3": "",
+           "4": ((), []),
+           "5": [[], ()]
+        }
+    })
+
+    assert is_none_or_empty({}, verify_iterable_values=False)
+    assert not is_none_or_empty({
+        "a": None, 
+        "b": [], 
+        "c": (),
+        "d": "",
+        "e": [[], ()],
+        "f": ((), []),
+        "g": {},
+        "h": {
+           "1" : None,
+           "2": {},
+           "3": "",
+           "4": ((), []),
+           "5": [[], ()]
+        }
+    }, verify_iterable_values=False)
+
+def test_not_supported_type():
+    for not_supported_type in NOT_SUPPORTED_TYPES:
+
+        if not_supported_type is range:
+            type_call = not_supported_type(1)
+        
+        elif not_supported_type is memoryview:
+            type_call = not_supported_type(b'm')
+            
+        else :   
+            type_call = not_supported_type()
+
+        str_type = str(type(type_call))
+
+        with raises(Exception, match=NOT_SUPPORTED_TYPES_EXCEPTION_MESSAGE.format(str_type)):
+            is_none_or_empty(type_call)

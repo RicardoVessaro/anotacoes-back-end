51549c8d0b9daa13f940d69837baa49f1f2138bb
ricardovessaro <ricardolvessaro@gmail.com>
2022-03-23 21:24:30 -0300

    testing ARQ CRUD Validatoor

--- arq/service/arq_crud_validator.py
+++ arq/service/arq_crud_validator.py
@@ -7,30 +7,40 @@ class ArqCRUDValidator:
     def __init__(self, required_fields=[]) -> None:
         self.required_fields = required_fields
 
-    def validate_insert(self, body: dict):
+    def validate_insert(self, body):
        self._validate_required_fields_on_insert(body)
 
-    def validate_update(self, id, body: dict):
+    def validate_update(self, id, body):
         self._validate_required_fields_on_update(body)
 
     def validate_delete(self, id):
         pass
 
     def _validate_required_fields_on_insert(self, body: dict):
-
-        for required_field in self.required_fields:
-            if required_field not in body.keys() or is_none_or_empty(body[required_field]):
-                self._raise_required_field_exception(required_field)
-
-    def _validate_required_fields_on_update(self, body):
         body_dict = body
 
         if not type(body) is dict:
             body_dict = body.to_mongo()
 
-        for field, value in body_dict.items():
+        for required_field in self.required_fields:
+            if required_field not in body_dict.keys() or is_none_or_empty(body_dict[required_field]):
+                self._raise_required_field_exception(required_field)
 
+    def _validate_required_fields_on_update(self, body):
+        if type(body) is dict:
+            self._validate_dict_required_fields_on_updade(body)
+
+        else :
+            self._validate_model_required_fields_on_updade(body)
+
+    def _validate_dict_required_fields_on_updade(self, body):
+        for field, value in body.items():
             if field in self.required_fields and is_none_or_empty(value):
+                self._raise_required_field_exception(field)
+
+    def _validate_model_required_fields_on_updade(self, body):
+        for field in self.required_fields:
+            if is_none_or_empty(body[field]):
                 self._raise_required_field_exception(field)
 
     def _raise_required_field_exception(self, required_field: str):
---
+++ arq/tests/service/arq_crud_validator_test.py
@@ -0,0 +1,123 @@
+
+from xmlrpc.client import boolean
+from pytest import raises
+from arq.exception.arq_exception_message import REQUIRED_FIELD_EXCEPTION_MESSAGE
+from arq.service.arq_crud_validator import ArqCRUDValidator
+from arq.exception.arq_exception import ArqException
+from arq.tests.resources.data.model.arq_test_model import ArqTestModel
+
+# TODO Testar ArqCRUDValidator
+# TODO Testar Usando Document e dict
+
+class TestArqCRUDValidator:
+
+    arq_crud_validator = ArqCRUDValidator(required_fields=['code', 'title'])
+
+    def test_validate_insert(self):
+
+        def test_using_dict_when_required_fields_are_not_given():
+
+            body = {
+                'code': 1,
+                'title': 'test_validate_insert_ArqCRUDValidator',
+                'boolean': True,
+                'tag': ['A', 'B']
+            }
+
+            for required_field in self.arq_crud_validator.required_fields:
+                poped_value = body.pop(required_field)
+
+                with raises(ArqException, match=REQUIRED_FIELD_EXCEPTION_MESSAGE.format(required_field)):
+                    self.arq_crud_validator.validate_insert(body)
+
+                body[required_field] = poped_value
+
+        test_using_dict_when_required_fields_are_not_given()
+
+        def test_using_dict_when_required_fields_are_empty():
+
+            body = {
+                'code': 1,
+                'title': 'test_validate_insert_ArqCRUDValidator',
+                'boolean': True,
+                'tag': ['A', 'B']
+            }
+
+            for required_field in self.arq_crud_validator.required_fields:
+                value = body[required_field]
+
+                body[required_field] = None
+
+                with raises(ArqException, match=REQUIRED_FIELD_EXCEPTION_MESSAGE.format(required_field)):
+                    self.arq_crud_validator.validate_insert(body)
+
+                body[required_field] = value
+
+        test_using_dict_when_required_fields_are_empty()
+
+        def test_using_model():
+
+            model = ArqTestModel(
+                code=1,
+                title='test_validate_insert_ArqCRUDValidator',
+                boolean=True,
+                tags=['A', 'B']
+            )
+
+            for required_field in self.arq_crud_validator.required_fields:
+                poped_value = model[required_field]
+                model[required_field] = None
+
+                with raises(ArqException, match=REQUIRED_FIELD_EXCEPTION_MESSAGE.format(required_field)):
+                    self.arq_crud_validator.validate_insert(model)
+
+                model[required_field] = poped_value
+
+        test_using_model()
+
+    def test_update(self):
+
+        def test_must_raise_exception_when_required_fields_are_empty_on_update_using_dict():
+            fake_id = "12AB3CD4"
+            
+            body = {
+                "title": None,
+                "pinned": True,
+                "text": "lorem ipsum dolor sit amet"
+            }
+
+            for field in body.keys():
+                if field in self.arq_crud_validator.required_fields:
+                    value = body[field]
+
+                    body[field] = None
+
+                    with raises(ArqException, match=REQUIRED_FIELD_EXCEPTION_MESSAGE.format(field)):
+                        self.arq_crud_validator.validate_update(fake_id, body)
+
+                    body[field] = value
+
+        test_must_raise_exception_when_required_fields_are_empty_on_update_using_dict()
+
+        def test_must_raise_exception_when_required_fields_are_empty_on_update_using_model():
+
+            fake_id = "12AB3CD4"
+        
+            model = ArqTestModel(
+                code=1, 
+                title='test_update_ArqCRUDValidator',
+            )
+
+            for field in model.to_mongo().keys():
+                if field in self.arq_crud_validator.required_fields:
+                    value = model[field]
+
+                    model[field] = None
+
+                    with raises(ArqException, match=REQUIRED_FIELD_EXCEPTION_MESSAGE.format(field)):
+                        self.arq_crud_validator.validate_update(fake_id, model)
+
+                    model[field] = value
+
+        test_must_raise_exception_when_required_fields_are_empty_on_update_using_model()
+

d236feb85e4a56d51e1b0cd4339ada9c78324ce7
ricardovessaro <ricardolvessaro@gmail.com>
2022-03-30 22:44:58 -0300

    creating ENUM Structure

--- arq/service/enum/enum_validator.py
+++ arq/service/enum/enum_validator.py
@@ -5,8 +5,6 @@ from arq.exception.exception_message import DUPLICATED_ENUM_CODE
 from arq.service.crud_validator import CRUDValidator
 from arq.util.object_util import is_none_or_empty
 
-# TODO Refatorar validador para nao validar o validate insert/update 2 vezes
-
 class EnumValidator(CRUDValidator):
 
     def __init__(self, dao, required_fields=[]) -> None:
@@ -18,7 +16,6 @@ class EnumValidator(CRUDValidator):
 
         for enum in enums:
             self._validate_duplicated_enum(enum, enums)
-            #self._validate_enum(enum)
             
     def _validate_duplicated_enum(self, enum, enums):
         count = 0
@@ -29,16 +26,3 @@ class EnumValidator(CRUDValidator):
 
             if count > 1:
                 raise ArqException(DUPLICATED_ENUM_CODE.format(enum.code))
-
-    """
-    def _validate_enum(self, enum):
-        db_enum = self._dao.find(code=enum.code)
-
-        enum_already_exists = not is_none_or_empty(db_enum)
-
-        if enum_already_exists:
-            self.validate_update(enum.id, enum)
-
-        else:
-            self.validate_insert(enum)
-    """

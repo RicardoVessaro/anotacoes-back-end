1d5504fdef7629343c4c27ee4d3c3fd96a6f1f76
ricardovessaro <ricardolvessaro@gmail.com>
2022-05-24 17:38:51 -0300

    setting default tag by code in note_service

--- api/modules/core/blueprints/service/note/note_service.py
+++ api/modules/core/blueprints/service/note/note_service.py
@@ -4,11 +4,14 @@ from api.modules.core.blueprints.data.dao.note_dao import NoteDAO
 from api.modules.core.blueprints.service.note.note_validator import NoteValidator
 from api.modules.core.blueprints.service.tag.tag_service import OK
 from arq.service.crud_service import CRUDService
+from api.modules.core.blueprints.service.tag.tag_service import TagService
 
 CREATED_IN = 'created_in'
 TAG = 'tag'
 
 class NoteService(CRUDService):
+
+    NAME = 'note'
 
     def __init__(self) -> None:
         dao = NoteDAO()
@@ -23,6 +26,7 @@ class NoteService(CRUDService):
         body[CREATED_IN] = datetime.datetime.today()
 
         if TAG not in body:
-            body[TAG] = OK.id
+            body[TAG] = TagService().find_by_code(code=OK.code).id
+            print(body[TAG])
 
         return super().insert(body)

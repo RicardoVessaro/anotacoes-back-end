0681649da025f9e9c41fa886c2c5204fb6ab3847
ricardovessaro <ricardolvessaro@gmail.com>
2022-06-08 19:18:42 -0300

    using cascade

--- api/annotation/data/dao/note_dao.py
+++ api/annotation/data/dao/note_dao.py
@@ -1,10 +1,21 @@
 
 from api.annotation.data.model.note import Note
+from api.annotation.data.dao.link_dao import LinkDAO
+from api.annotation.data.dao.picture_dao import PictureDAO
+from ipsum.data.cascade import Cascade
 
 from ipsum.data.dao.crud_dao import CRUDDAO
 
 class NoteDAO(CRUDDAO):
 
     def __init__(self) -> None:
-        super().__init__(Note)
+        super().__init__(
+            model=Note,
+            cascade=Cascade(
+                childs=[
+                    LinkDAO(),
+                    PictureDAO()
+                ]
+            )
+        )
     
--- api/annotation/data/dao/picture_dao.py
+++ api/annotation/data/dao/picture_dao.py
@@ -1,8 +1,17 @@
 
+from ipsum.data.cascade import Cascade
 from ipsum.data.dao.detail_crud_dao import DetailCRUDDAO
 from api.annotation.data.model.picture import Picture
+from api.annotation.data.dao.comment_dao import CommentDAO
 
 class PictureDAO(DetailCRUDDAO):
 
     def __init__(self) -> None:
-        super().__init__(Picture)
+        super().__init__(
+            model=Picture,
+            cascade=Cascade(
+                childs=[
+                    CommentDAO()
+                ]
+            )
+        )

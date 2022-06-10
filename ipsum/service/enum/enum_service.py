7c272691f0ad375ad436c5e7d2e2dca9c023eadf
ricardovessaro <ricardolvessaro@gmail.com>
2022-06-10 20:07:28 -0300

    adding logger in enum_service

--- ipsum/service/enum/enum_service.py
+++ ipsum/service/enum/enum_service.py
@@ -1,6 +1,7 @@
 
 from ipsum.data.model.enum_document import CODE, NAME, EnumDocument
 from ipsum.service.crud_service import CRUDService
+from ipsum.util.logger import Logger
 from ipsum.util.object_util import is_none_or_empty
 
 class EnumService(CRUDService):
@@ -14,18 +15,26 @@ class EnumService(CRUDService):
         self.enums = enums
 
     def save_enums(self):
+        logger = Logger(self)
+
         self.validator.validate_enums(self.enums)
+
+        logger.info(f'Saving ENUMs')
 
         for enum in self.enums:
             
             db_enum_result = self.find(code=enum.code)
 
             if is_none_or_empty(db_enum_result):
+                logger.info(f'Inserting {enum.code} - {enum.name}')
+
                 self.insert(enum)
 
             else:
                 db_enum = db_enum_result.first()
                 enum.id = db_enum.id
+
+                logger.info(f'Updating {enum.code} - {enum.name}')
 
                 self.update(enum.id, enum)
 

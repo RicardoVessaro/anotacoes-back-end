057731a57136579b599cb8a8f12552caf43d73e8
ricardovessaro <ricardolvessaro@gmail.com>
2022-06-11 12:28:10 -0300

    mudando mensagem do run

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+from flask import request
 from api.api import create_flask_app
 
 api = create_flask_app()
@@ -5,7 +6,7 @@ api = create_flask_app()
 
 @api.route("/")
 def root():
-    return "API esta rodando !!!"
+    return f"API esta rodando em {request.host_url}"
 
 if __name__=='__main__':
     api.run()

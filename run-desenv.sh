93a01a8ba38e954fffdbab70f3b270333e47abe2
ricardovessaro <ricardolvessaro@gmail.com>
2022-06-23 06:26:46 -0300

    creating script to run flask locally

---
+++ run-desenv.sh
@@ -0,0 +1,7 @@
+source setup-flask.sh
+
+export FLASK_CONFIG=development.cfg
+
+export FLASK_CONFIG_TEST=test.cfg
+
+python -m run
---
+++ run-test.sh
@@ -0,0 +1,6 @@
+
+export FLASK_CONFIG=development.cfg
+
+export FLASK_CONFIG_TEST=test.cfg
+
+python -m run FLASK_CONFIG_TEST

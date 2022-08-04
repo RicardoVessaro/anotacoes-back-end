cf3b5fcffe5dddfa456e9706eb3a8d439e470111
ricardovessaro <ricardolvessaro@gmail.com>
2022-08-03 22:19:06 -0300

    inserting ids in enums

--- api/annotation/service/mood/mood_service.py
+++ api/annotation/service/mood/mood_service.py
@@ -6,12 +6,12 @@ from ipsum.service.enum.ipsum_enum import ipsum_enum
 from ipsum.service.enum.enum_service import EnumService
 
 
-COOL = Mood(code=1, name='Cool')
-OK = Mood(code=2, name='Ok')
-BORING = Mood(code=3, name='Boring')
-SAD = Mood(code=4, name='Sad')
-LOVE = Mood(code=5, name='Love')
-GREAT = Mood(code=6, name='Great')
+COOL = Mood(id='62b356430d2ae186982e96ef', code=1, name='Cool')
+OK = Mood(id='62b356430d2ae186982e96f0', code=2, name='Ok')
+BORING = Mood(id='62b356430d2ae186982e96f1', code=3, name='Boring')
+SAD = Mood(id='62b356430d2ae186982e96f2', code=4, name='Sad')
+LOVE = Mood(id='62b356430d2ae186982e96f3', code=5, name='Love')
+GREAT = Mood(id='62b356430d2ae186982e96f4', code=6, name='Great')
 
 @ipsum_enum()
 class MoodService(EnumService):
--- api/annotation/service/tag/tag_service.py
+++ api/annotation/service/tag/tag_service.py
@@ -5,9 +5,9 @@ from api.annotation.service.tag.tag_validator import TagValidator
 from ipsum.service.enum.ipsum_enum import ipsum_enum
 from ipsum.service.enum.enum_service import EnumService
 
-IMPORTANT = Tag(code=1, name="IMPORTANT", priority=1)
-OK = Tag(code=2, name="OK", priority=2)
-LATER = Tag(code=3, name="LATER", priority=3)
+IMPORTANT = Tag(id='62df2767730a8633f2fec97f', code=1, name="IMPORTANT", priority=1)
+OK = Tag(id='62b3565f0d2ae186982e9746', code=2, name="OK", priority=2)
+LATER = Tag(id='62b356600d2ae186982e9747', code=3, name="LATER", priority=3)
 
 @ipsum_enum()
 class TagService(EnumService):   
